39be58755069dec5a21f53574aa8fbf9505bc8c2
hnassri <46187243+hnassri@users.noreply.github.com>
2019-01-30 22:07:10 +0100

    ajout coms

--- db/migrate/20190130171150_create_lectures.rb
+++ db/migrate/20190130171150_create_lectures.rb
@@ -1,3 +1,5 @@
+# On crée la table lecture avec comme paramètres un title en string, une description en text et un timestamp
+
 class CreateLectures < ActiveRecord::Migration[5.2]
   def change
     create_table :lectures do |t|
