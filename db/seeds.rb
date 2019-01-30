6fba5e0bd81099b2345b94130b5eae36ecc47e55
hnassri <46187243+hnassri@users.noreply.github.com>
2019-01-30 22:13:25 +0100

    ajout coms

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
-
+# On crée 3 cours.
 3.times do 
   lecture = Lecture.create!(title: Faker::HarryPotter.house, description: Faker::HarryPotter.quote)
 end
-
+# On crée 10 leçons pour les cours précèdemment créer
 10.times do
   lesson = Lesson.create!(title: Faker::HarryPotter.spell, body: Faker::HarryPotter.book,lecture_id: rand(1..4))
 end
