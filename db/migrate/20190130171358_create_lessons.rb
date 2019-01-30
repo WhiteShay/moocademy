ae64a62de94f6abf9b14345927627cf2bcca49a4
hnassri <46187243+hnassri@users.noreply.github.com>
2019-01-30 22:10:57 +0100

    ajout coms

--- db/migrate/20190130171358_create_lessons.rb
+++ db/migrate/20190130171358_create_lessons.rb
@@ -1,3 +1,5 @@
+# On crée la table lessons avec pour attributs un title en string, un body en text et un timestamps. On n profite pour le lier à lecture
+
 class CreateLessons < ActiveRecord::Migration[5.2]
   def change
     create_table :lessons do |t|
